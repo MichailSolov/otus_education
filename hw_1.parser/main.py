6484ccc266ed17b8eac1832a7e0a36e7ba4074aa
mihailsolovev <michail_solov2001@mail.ru>
2022-11-24 17:49:05 +0300

    added file output

--- hw_1.parser/main.py
+++ hw_1.parser/main.py
@@ -3,27 +3,34 @@ import bs4
 import optparse
 import pprint
 
-avito_url = "http://127.0.0.1/avito_main.html"
+avito_url_local = "http://127.0.0.1/avito_main.html"
 sneakers_url = "http://127.0.0.1/avito_second.html"
-
-
-# url = "https://www.avito.ru"
+avito_url_global = "https://www.avito.ru"
 
 
 class Parser:
-    def __init__(self, parser: optparse.OptionParser, url):
+    def __init__(self, parser: optparse.OptionParser):
         (self.options, _) = parser.parse_args()
 
-        self.url = url
         self.count = self.options.count
         self.debug = self.options.debug
         self.file_path = self.options.file_path
+        self.fp = None
+        self.url = avito_url_global if not self.debug else avito_url_local
 
     def extract_adv(self):
+        print("Начало обработки основной страницы продукта", file=self.fp)
         html = requests.get(self.url).text
         soap = bs4.BeautifulSoup(html, "html.parser")
-        if self.debug:
+        if self.debug == 2:
             print(soap)
+        try:
+            if "<h1>Доступ ограничен: проблема с IP</h1>" == str(soap.find_all('h1')[0]):
+                print("Доступ ограничен: проблема с IP")
+                return
+        except IndexError:
+            pass
+
         advertisements = soap.find_all('a', attrs={"class": ["link-link-MbQDP",
                                                              "link-design-default-_nSbv",
                                                              "title-root-zZCwT",
@@ -34,10 +41,14 @@ class Parser:
             self.additional_adv(advertisements[i].get("href"))
 
     def additional_adv(self, url):
-        url = sneakers_url
+
+        print("\n\n\nНачало обработки дополнительной страницы продукта\n\n\n", file=self.fp)
+
+        url = sneakers_url if self.debug else self.url + url
+
         html = requests.get(url).text
         soap = bs4.BeautifulSoup(html, "html.parser")
-        if self.debug:
+        if self.debug == 2:
             print(soap)
         advertisements = soap.find_all('a', attrs={"class": ["link-link-MbQDP",
                                                              "link-design-default-_nSbv",
@@ -45,18 +56,22 @@ class Parser:
                                                              "iva-item-title-py3i_",
                                                              "title-root_maxHeight-X6PsH"]})
         for i in advertisements:
-            print(i.get("href"))
+            print("Ссылки на дополнительной странице продукт: ", i.get("href"), file=self.fp)
 
     def run(self):
-        self.extract_adv()
+        try:
+            with open(self.file_path, "w+") as self.fp:
+                self.extract_adv()
+        except TypeError:
+            self.extract_adv()
 
 
 def main():
     parser = optparse.OptionParser()
     parser.add_option("-c", "--count", dest="count", help="Number of extracted advertisement.", type="int")
     parser.add_option("-f", "--file", dest="file_path", help="Write results to file (default none)", default=None)
-    parser.add_option("-d", "--debug", dest="debug", action="store_true", help="Write out debug info")
-    avito_parser = Parser(parser, avito_url)
+    parser.add_option("-d", "--debug", dest="debug", help="Write out debug info", type="int")
+    avito_parser = Parser(parser)
     avito_parser.run()
 
 
